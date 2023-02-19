1d95eaca88eae536307a922afdfc10efd82a90e9
m3r <frzwis@protonmail.ch>
2023-02-19 16:37:42 +0300

    changed uuid to 10 characters instead of 4

--- stringutils.py
+++ stringutils.py
@@ -1,7 +1,7 @@
 import uuid
-
+#Yeah I know it does not return four but 10
 def randomFour():
-    return str(uuid.uuid4())[:4]
+    return str(uuid.uuid4())[:11].replace("-", "")
 
 def randomEight():
     return str(uuid.uuid4())[:8]
