245f8d4191b24e7223ff962552b9a01341ba501f
m3r <frzwis@protonmail.ch>
2023-03-15 00:19:24 +0300

    v0.02.0beta uwsgi compatible

--- README.md
+++ README.md
@@ -1,51 +1,64 @@
-# busfs-server
+# BUSFS-server
 
 Basically Useless Simple File Storage is a very minimalist server side code with a web ui that lets you upload files, view uploaded files, and lets you download and delete those files. It is protected by basic HTTP authentication with an easy to set config. It is written in Python using Flask.
-
-***Warning:*** This app currently does not have a secure way to open to WAN as it does not have a WSGI server configuration yet. Use this only in your local network for the time being.
 
 For more documentation, please visit the wiki.
 
 ### Features
-- Collision protection: busfs appends a 10 character string to the end of your file to avoid collisions if the files have same names. Additionally, it just checks whether the uuid exists and assigns a new one.
+- Collision protection: BUSFS appends a 10 random character string to the end of your file to avoid collisions if the files have same names. Additionally, it just checks whether the uuid exists and assigns a new one.
 
 - Descriptions: You can add and update descriptions of the files that you are uploading for more information.
 
-- Security: busfs forces basic HTTP authentication for the entire app. Soon, there will be token based authentication and maybe even multi user support for shared and private directories later on.
+- Security: BUSFS forces basic HTTP authentication for the entire app. Soon, there will be token based authentication and maybe even multi user support for shared and private directories later on.
 
-- Clients: After busfs is in working order, clients for different platforms (Linux, Windows, maybe even Android) will be developed with both CLI and GUI alternatives. It is also very easy to integrate your own apps as all the API's are easily accessible.
+- Clients: After BUSFS is in working order, clients for different platforms (Linux, Windows, maybe even Android) will be developed with both CLI and GUI alternatives. It is also very easy to integrate your own apps as all the API's are easily accessible.
 
 - Simple with no maintenance: To run, you only have to install the dependencies and run a single file.
 
 ### Limitations
-- Currently, busfs stores the user password in plaintext in the app.py file. Which means your username and password will reset if you just copy and paste the new version of this app in place of the old one. Regarding plaintext, it should not be an issue as someone who gets access to your server already has access to your files. Please encrypt your files if this is a concern.
+- Currently, BUSFS stores the user password in plaintext in the app.py file. Which means your username and password will reset if you just copy and paste the new version of this app in place of the old one. Regarding plaintext, it should not be an issue as someone who gets access to your server already has access to your files. Please encrypt your files if this is a concern.
 
 - It supports only one user account. 
 
 - UI could be better. I am working on it.
 
 ---
-### Installation
-After you install, the username and password will be generated randomly and you can find and edit them and host and port settings in the config.ini file.
+### Installing BUSFS
+On your first run, the username and password will be generated randomly and you can find and edit them in the config.ini file.
 
 From Codeberg
 1. Install `git`, and `python3`.
-2. Run `git clone https://codeberg.org/m3r/busfs-server.git` in your terminal
-3. Run `cd busfs-server`
-4. Run `python3 -m venv venv`
-5. Run `source venv/bin/activate`
-6. Run `python3 -m pip install -r requirements.txt`
-7. Run `python3 app.py` to start the app.
+2. Run `git clone https://codeberg.org/m3r/BUSFS-server.git` in your terminal to clone the repo
+3. Run `cd BUSFS-server`
+4. Run `python3 -m venv venv` to create the virtual environment
+5. Run `source venv/bin/activate` to activate the virtual environment (`deactivate` to deactivate it)
+6. Run `python3 -m pip install -r requirements.txt` to install the dependencies
    
 From script
-1. Run `curl https://codeberg.org/m3r/busfs-server/raw/branch/master/install.sh | sh` (works with `apt`, `dnf`, `zypper`, `pacman`, `brew`, `apk`, and `yum`)
-2. Run `python3 app.py` to start the app.
+1. Run `curl https://codeberg.org/m3r/BUSFS-server/raw/branch/master/install.sh | sh` (works with `apt`, `dnf`, `zypper`, `pacman`, `brew`, `apk`, and `yum`)
 
 Using Docker
 1. (soon)
 
 Using an executable
 1. (soon)
+
+### Starting BUSFS
+
+If you've installed from Codeberg or the script, run `uwsgi --http-socket 0.0.0.0:5000 --wsgi-file start.py` (always be in the virtual environment when running. The script puts you in one).
+
+You can adjust the host and port by modifying the command above
+
+### Updating BUSFS
+
+soon:tm:
+
+#### To consider
+It should be noted that if you put BUSFS behind a reverse proxy, do not forget to increase the maximum body size (guides for [nginx](https://nginx.org/en/docs/http/ngx_http_core_module.html#client_max_body_size), [apache](https://ubiq.co/tech-blog/increase-file-upload-size-apache), [caddy](https://caddyserver.com/docs/modules/http.handlers.request_body), [lighttpd](https://redmine.lighttpd.net/projects/lighttpd/wiki/Server_max-request-sizeDetails)) otherwise you will get a 413 error.
+
+By default, update checking is disabled. You can enable it by typing `yes` instead of `no` in config.ini file.
+
+You may want to use [`screen`](https://wiki.archlinux.org/title/GNU_Screen) or daemonize uwsgi by adding `--daemonize logfile.log` to the start command to keep uwsgi running in the background.
 
 ### TODO / Roadmap
 - [x] Manage configuration through a config.ini file
@@ -58,6 +71,7 @@ Using an executable
 - [x] Switch to 10 digit UUID's instead of 4
 - [ ] Switch to Flask-Security
 - [x] Add versions and releases to the app
+- [ ] Write a wiki
 
 
 ### "Maybe" features
--- app.py
+++ app.py
@@ -9,96 +9,104 @@ from datetime import date
 from shutil import rmtree
 import configparser
 
-VERSION = "v0.01.0beta"
+VERSION = "v0.02.0beta"
 
-config = configparser.ConfigParser()
-if not os.path.isfile("config.ini"):
-    config['settings'] = {
-    'USERNAME': sutil.randomTen(),
-    'PASSWORD': sutil.randomTen(),
-    'HOST': '0.0.0.0',
-    'PORT': '6798',
-    'CHECK-FOR-UPDATES' : "yes" if input("Do you want to check for updates automatically? y/N ") in ["y", "Y", "yes"] else "no"
-    }
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
+def create_app():
+    config = configparser.ConfigParser()
+    if not os.path.isfile("config.ini"):
+        config['settings'] = {
+        'USERNAME': sutil.randomTen(),
+        'PASSWORD': sutil.randomTen(),
+        #'HOST': '0.0.0.0', Enable for debugging purposes for when using the built in flask server
+        #'PORT': '6798',
+        'CHECK-FOR-UPDATES' : "no"
+        }
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
 
-config.read('config.ini')
-USERNAME = str(config.get('settings', 'username'))
-PASSWORD = str(config.get('settings', 'password'))
-HOST = str(config.get('settings', 'host'))
-PORT = int(config.get('settings', 'port'))
-CHKFORUPDATES = str(config.get('settings', 'check-for-updates'))
+    config.read('config.ini')
+    USERNAME = str(config.get('settings', 'username'))
+    PASSWORD = str(config.get('settings', 'password'))
+    #HOST = str(config.get('settings', 'host'))
+    #PORT = int(config.get('settings', 'port'))
+    CHKFORUPDATES = str(config.get('settings', 'check-for-updates'))
 
-if CHKFORUPDATES == "yes":
-    response = requests.get(f'https://codeberg.org/api/v1/repos/m3r/busfs-server/releases')
-    release = response.json()
-    if response.status_code == 200:
-        if len(release) > 0 and str(release[0]['tag_name']) != VERSION:
-            print(f'Your version is {VERSION}, and the latest version is {str(release[0]["tag_name"])}.')
+    if CHKFORUPDATES == "yes":
+        response = requests.get(f'https://codeberg.org/api/v1/repos/m3r/busfs-server/releases')
+        release = response.json()
+        if response.status_code == 200:
+            if len(release) > 0 and str(release[0]['tag_name']) != VERSION:
+                versionmessage = f'Your version is "{VERSION}", and the latest version is "{str(release[0]["tag_name"])}".' #print(f'Your version is {VERSION}, and the latest version is {str(release[0]["tag_name"])}.')
+            else:
+                versionmessage = "You are running the latest version of this app" #print(f'You are running the latest version of this app')
         else:
-            print(f'You are running the latest version of this app')
+            versionmessage = f'Something went wrong while checking for updates. Response code is {int(response.status_code)}. Your version is "{VERSION}" and the upstream is "{str(release[0]["tag_name"])}"' #print(f'Something went wrong while checking for updates. Response code is {int(response.status_code)}. Your version is {VERSION} and the upstream is {str(release[0]["tag_name"])}')
     else:
-        print(f'Something went wrong while checking for updates. Response code is {int(response.status_code)}. Your version is {VERSION} and the upstream is {str(release[0]["tag_name"])}')
+        versionmessage = ""
 
-app = Flask(__name__)
+    datapath = 'data'
+    
+    app = Flask(__name__)
 
-app.config['BASIC_AUTH_USERNAME'] = USERNAME
-app.config['BASIC_AUTH_PASSWORD'] = PASSWORD
-app.config['BASIC_AUTH_FORCE'] = True
-basic_auth = BasicAuth(app)
-datapath = 'data'
+    app.config['BASIC_AUTH_USERNAME'] = USERNAME
+    app.config['BASIC_AUTH_PASSWORD'] = PASSWORD
+    app.config['BASIC_AUTH_FORCE'] = True
+    basic_auth = BasicAuth(app)
 
-if not os.path.exists(datapath):
-    os.makedirs(datapath)
+    if not os.path.exists(datapath):
+        os.makedirs(datapath)
 
-@app.route("/")
-def index():
-    fileNamesList, descriptionList, uploadDateList, uuidList = dutil.loadData()
-    return render_template("index.html", fileNamesList=fileNamesList, descriptionList=descriptionList, uploadDateList=uploadDateList, uuidList=uuidList)
+    @app.route("/")
+    def index():
+        fileNamesList, descriptionList, uploadDateList, uuidList = dutil.loadData()
+        return render_template("index.html", fileNamesList=fileNamesList, descriptionList=descriptionList, uploadDateList=uploadDateList, uuidList=uuidList, versionmessage=versionmessage)
 
-@app.post("/upload-file")
-def upload():
+    @app.post("/upload-file")
+    def upload():
 
-    file = request.files['userfile']
-    if file == None:
-        return("400", 400)
+        file = request.files['userfile']
+        if file == None:
+            return("400", 400)
 
-    desc = request.form['userdescription']
+        desc = request.form['userdescription']
 
-    randstr = sutil.randomTen()
-
-    while os.path.isdir(randstr):
         randstr = sutil.randomTen()
 
-    os.makedirs(f"{datapath}/{randstr}")
-    file.save(f'{datapath}/{randstr}/{file.filename}')
-    today = date.today()
-    todayformat = today.strftime("%d.%m.%Y")
-    jsontowrite = {
-        "file_name": file.filename,
-        "date_uploaded": todayformat,
-        "description": desc,
-        "uuid": randstr,
-    }
-    with open(f'{datapath}/{randstr}/data.json', 'w') as f:
-        json.dump(jsontowrite, f)
+        while os.path.isdir(randstr):
+            randstr = sutil.randomTen()
 
-    return redirect("/")
+        os.makedirs(f"{datapath}/{randstr}")
+        file.save(f'{datapath}/{randstr}/{file.filename}')
+        today = date.today()
+        todayformat = today.strftime("%d.%m.%Y")
+        jsontowrite = {
+            "file_name": file.filename,
+            "date_uploaded": todayformat,
+            "description": desc,
+            "uuid": randstr,
+        }
+        with open(f'{datapath}/{randstr}/data.json', 'w') as f:
+            json.dump(jsontowrite, f)
 
-@app.route('/download/<uuid>')
-def download(uuid):
-    return send_from_directory(f"data/{uuid}", dutil.getFile(uuid), as_attachment=True)
+        return redirect("/")
 
-@app.route("/delete/<uuid>")
-def delete(uuid):
-    rmtree(dutil.getDir(uuid))
-    return redirect("/")
+    @app.route('/download/<uuid>')
+    def download(uuid):
+        return send_from_directory(f"data/{uuid}", dutil.getFile(uuid), as_attachment=True)
 
-@app.post('/update-description/<uuid>')
-def update(uuid):
-    desc = request.form['inlinedesc']
-    dutil.updateDesc(uuid, desc)
-    return redirect("/")
+    @app.route("/delete/<uuid>")
+    def delete(uuid):
+        rmtree(dutil.getDir(uuid))
+        return redirect("/")
 
-app.run(debug=True, host= HOST, port=PORT)
+    @app.post('/update-description/<uuid>')
+    def update(uuid):
+        desc = request.form['inlinedesc']
+        dutil.updateDesc(uuid, desc)
+        return redirect("/")
+
+    #app.run(debug=True, host= HOST, port=PORT)
+
+    return app
+
+#create_app()
